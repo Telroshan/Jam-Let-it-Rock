5277fbbfddc583869f36b12aaa9a1baef083dfa0
biblion84 <biblion84@free.fr>
2019-11-23 12:15:22 +0100

    Timer's OK

--- Assets/Scripts/CountDownManager.cs
+++ Assets/Scripts/CountDownManager.cs
@@ -2,30 +2,53 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountDownManager : MonoBehaviour
 {
-    public float StartTime = 5;
-
+    public float StartTimeBase = 5;
+    private float _remainningTime;
+    private bool _countdownInProgress = false;
     public TextMeshProUGUI TextMeshProUgui;
+    public Image LoadingBar;
     
     // Start is called before the first frame update
     void Start()
     {
-        TextMeshProUgui.text = StartTime.ToString("0.00");
+        StartCountDown();
+    }
+
+    public void StartCountDown()
+    {
+        TextMeshProUgui.text = StartTimeBase.ToString("0.00");
+        _remainningTime = StartTimeBase;
+        _countdownInProgress = true;
+    }
+
+    private void OnTimeUp()
+    {
+        Debug.Log("Time's UP");
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartTime -= Time.deltaTime;
-        if (StartTime <= 0)
+        if (_countdownInProgress)
         {
-            StartTime = 0;
-            //TODO: Call Next Round
+            _remainningTime -= Time.deltaTime;
+            if (_remainningTime <= 0)
+            {
+                _remainningTime = 0;
+                _countdownInProgress = false;
+                //TODO: Call Next Round
+                OnTimeUp();
+            }
 
+            LoadingBar.fillAmount = Mathf.Clamp01(_remainningTime / StartTimeBase);
+            
+            TextMeshProUgui.text = _remainningTime.ToString("0.00");
         }
-        TextMeshProUgui.text = StartTime.ToString("0.00");
+       
 
     }
 }

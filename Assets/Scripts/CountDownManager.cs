2de1994ff5e2c279ef8d405c5553a7e73dbdc5bd
biblion84 <biblion84@free.fr>
2019-11-23 11:54:34 +0100

    Timer OK

---
+++ Assets/Scripts/CountDownManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CountDownManager : MonoBehaviour
+{
+    public float StartTime = 5;
+
+    public TextMeshProUGUI TextMeshProUgui;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextMeshProUgui.text = StartTime.ToString("0.00");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartTime -= Time.deltaTime;
+        if (StartTime <= 0)
+        {
+            StartTime = 0;
+            //TODO: Call Next Round
+
+        }
+        TextMeshProUgui.text = StartTime.ToString("0.00");
+
+    }
+}

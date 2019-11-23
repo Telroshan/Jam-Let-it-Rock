958e2cf703eced602a7427d644cdf54a9459ddba
Vincent <vichenzo-thebaud@hotmail.com>
2019-11-23 17:07:58 +0100

    Menu scene

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu
+{
+    public class Menu : MonoBehaviour
+    {
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        public void PlayGame()
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}

f42cb97c2c247be3686aed1601c4f91dbf049dc0
jennifergllg <jennifer.gullung@ynov.com>
2019-11-23 16:34:43 +0100

    Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}

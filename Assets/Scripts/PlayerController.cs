35c4e0b2782b9a7853adc04e93591ec30682f6fa
Vincent <vichenzo-thebaud@hotmail.com>
2019-11-23 11:53:19 +0100

    Player inputs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public void Rock(InputAction.CallbackContext callbackContext)
+    {
+        if (!callbackContext.performed) return;
+        Debug.Log("ROCK");
+    }
+    public void Paper(InputAction.CallbackContext callbackContext)
+    {
+        if (!callbackContext.performed) return;
+        Debug.Log("PAPER");
+    }
+    public void Scissors(InputAction.CallbackContext callbackContext)
+    {
+        if (!callbackContext.performed) return;
+        Debug.Log("SCISSORS");
+    }
+}

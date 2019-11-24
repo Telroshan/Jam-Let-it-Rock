13a14a1a56e81c3fb75abda71c6597f768153d30
Vincent <vichenzo-thebaud@hotmail.com>
2019-11-24 19:55:47 +0100

    Update CameraShake.cs

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -9,10 +9,10 @@ public class CameraShake : MonoBehaviour
     public float duration = 1f;
     public float magnitude = 600f;
     public CountDownManager countDownManager;
-    
-    
+
+
     private bool _currentlyShaking = false;
-    
+
     public void Shake()
     {
         if (_currentlyShaking) return;
@@ -27,23 +27,23 @@ public class CameraShake : MonoBehaviour
 
     private IEnumerator ShakeCamera(float duration, float magnitude)
     {
-        Vector3 originalPos = transform.position;
-        
+        transform.localPosition = new Vector3(0, 0, 0);
+        Vector3 originalPos = new Vector3(0, 0, 0);
+
         float elapsed = 0f;
 
         while (elapsed < duration)
         {
-
             float x = Random.Range(-1f, 1f) * magnitude * 0.1f;
             float y = Random.Range(-1f, 1f) * magnitude * 0.1f;
-            
+
             transform.localPosition = new Vector3(x, y, originalPos.z);
             elapsed += Time.deltaTime;
-            
+
             yield return null;
         }
 
+        transform.localPosition = new Vector3(0, 0, 0);
         _currentlyShaking = false;
-        transform.localPosition = new Vector3(0,0,0);
     }
 }
